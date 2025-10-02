75761cec3457f2f8190276e77730a5b74a366495
mrgarbett <145857636+mrgarbett@users.noreply.github.com>
2025-10-02 14:21:29 +0100

    .

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('stagecoach-spoof-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './ttfs/student.svg',
+        // Add other assets as needed
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== 'stagecoach-spoof-v1').map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
